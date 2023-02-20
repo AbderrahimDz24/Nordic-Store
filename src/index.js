5d92ded6efc10cd273c996a659ed74efcb93da45
Abderrahim HELLACI <abderrahimdz24@gmail.com>
2023-02-20 15:40:12 +0100

    fetch slides dynamically

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import BodyParser from "koa-bodyparser";
 import Router from "koa-router";
 import Logger from "koa-logger";
 import serve from "koa-static";
+import * as nodeFetch from "node-fetch";
+import HttpStatus from "http-status";
+import {createApi} from "unsplash-js";
+
+let UNSPLASH_ACCESS_KEY = getVarEnv("UNSPLASH_ACCESS_KEY");
+
+const unsplashApi = createApi({
+  accessKey: UNSPLASH_ACCESS_KEY,
+  fetch: nodeFetch.default,
+});
 
 const app = new Koa();
 
@@ -15,6 +25,34 @@ app.use(serve("./public"));
 
 const router = new Router();
 
+router.get("/api/slides", async (ctx, next) => {
+  try {
+    ctx.status = HttpStatus.OK;
+    ctx.body = await getRandomPhotos();
+    await next();
+  } catch (e) {
+    ctx.status = HttpStatus.BAD_GATEWAY;
+    ctx.body = {
+      error_message: e.message
+    };
+  }
+
+  async function getRandomPhotos() {
+    let res = await unsplashApi.photos.getRandom({
+      count: 3,
+      query: "toys",
+      orientation: "landscape"
+    });
+    if (res?.errors?.length) {
+      throw new Error(`Unsplash Api Errors: ${res.errors.join()}`)
+    }
+    return res.response.map((p) => ({
+      description: p.description ?? p.alt_description ?? "",
+      url: p.urls.full,
+    }));
+  }
+});
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, function () {
@@ -24,3 +62,11 @@ app.listen(PORT, function () {
     PORT
   );
 });
+
+
+function getVarEnv(name) {
+  if (!process.env[name]) {
+    throw Error(`The var env ${name} is not defined.`);
+  }
+  return process.env[name];
+}
