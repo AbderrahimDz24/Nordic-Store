9f7eedfb389cee98c0dccabd5bd1bdd01ce97f0a
Abderrahim HELLACI <abderrahimdz24@gmail.com>
2023-02-23 07:14:48 +0100

    render slides and products on the server side.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import HttpStatus from "http-status";
 import {getRandomPhotos} from "./unsplach.js";
 import {apiHandler} from "./apiHandler.js";
 import {errorHandler} from "./errorHandler.js";
-
+import ejs from "koa-ejs";
+import * as path from "path";
+import {fileURLToPath} from 'url';
 
 const app = new Koa();
 
@@ -17,19 +19,22 @@ app.on('error', errorHandler);
 app.use(BodyParser());
 app.use(Logger());
 
+ejs(app, {
+  root: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../template'),
+  layout: false,
+  viewExt: 'ejs',
+  cache: false,
+  debug: false
+});
+
+
 app.use(serve("./public"));
 
 const router = new Router();
 
-router.get("/api/slides", apiHandler(async (ctx, next) => {
-  ctx.status = HttpStatus.OK;
-  ctx.body = await getRandomPhotos({count: 3, query: "toys"});
-  await next();
-}));
-
-router.get("/api/products", apiHandler(async (ctx, next) => {
-  ctx.status = HttpStatus.OK;
-  ctx.body = (await getRandomPhotos({
+router.get("/", async (ctx, next) => {
+  let slides = await getRandomPhotos({count: 3, query: "toys"});
+  let products = (await getRandomPhotos({
     count: 8,
     query: "toys",
     orientation: "squarish",
@@ -39,8 +44,11 @@ router.get("/api/products", apiHandler(async (ctx, next) => {
     price: Math.round((Math.random() * 89 + 10) * 100) / 100,
     picture_url: p.url
   }));
-  await next();
-}));
+  await ctx.render('index', {
+    slides,
+    products
+  })
+})
 
 app.use(router.routes()).use(router.allowedMethods());
 
